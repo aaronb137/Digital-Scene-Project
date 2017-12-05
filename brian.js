14aa544f66449023a826c712ad448ae1c39ebfec
Brian Ledesma <34288413+BrianDarkskil@users.noreply.github.com>
2017-12-05 15:35:31 -0800

    Create brian.js

---
+++ brian.js
@@ -0,0 +1,39 @@
+
+// Prepare Turtle to Draw
+hide();
+penUp();
+
+// First draw the background
+//drawBackground();
+
+//drawSky
+moveTo(160,80);
+
+//drawBackground();
+  penColor("Skyblue");
+  dot(500);
+  penUp(0);
+  moveForward(0);
+penWidth(150);
+
+//drawBirds
+moveTo(110,50);
+
+//drawBirds();
+penColor("Black");
+dot(10);
+arcRight(90, 25);
+penUp(2);
+moveForward();
+penWidth(200);
+
+//drawBirds();
+moveTo(170,250);
+
+//drawBirds();
+penColor("Black");
+dot(10);
+arcRight(10, 250);
+penUp(0);
+moveForward();
+penWidth(100);
