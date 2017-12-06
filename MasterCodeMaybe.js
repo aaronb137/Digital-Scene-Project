e84a4a1650500d5ce068037f93672665fab592f7
Michael Carranza <mike.carranza610@gmail.com>
2017-12-06 15:59:46 -0800

    Update MasterCodeMaybe.js

--- MasterCodeMaybe.js
+++ MasterCodeMaybe.js
@@ -1,37 +1,27 @@
-// First draw the background
-//drawBackground();
 hide();
-//drawSky
-moveTo(160,80);
-
-//drawBackground();
-  penColor("Skyblue");
-  dot(500);
-  penUp(0);
-  moveForward(0);
-  penWidth(150);
+drawSkyBackground();
 drawAllHills();
-penUp();
 drawSun();
 drawAllCactuses();
 drawAllBirds();
+
 function drawBirds() {
- moveTo(randomNumber(0, 320),randomNumber(0, 145));
- penDown();
- turnRight(randomNumber(90, 100));
- penWidth(7);
-penColor("Black");
- arcLeft(90, 8);
-arcRight(160, 10);
-dot(3);
-turnRight(270);
-arcRight(160, 10);
- arcLeft(90, 8);
-penUp(); 
+  moveTo(randomNumber(0, 320),randomNumber(0, 145));
+  penDown();
+  turnRight(randomNumber(90, 100));
+  penWidth(7);
+  penColor("Black");
+  arcLeft(90, 8);
+  arcRight(160, 10);
+  dot(3);
+  turnRight(270);
+  arcRight(160, 10);
+  arcLeft(90, 8);
+  penUp(); 
 }
 function drawAllBirds() {
   for (var i = 0; i < 4; i++) {
-    turnTo(-29);
+    turnTo(-210);
     drawBirds();
   }
 }
@@ -49,26 +39,38 @@ function draw1Hill() {
 }
 function drawBottomHill() {
   moveTo(-30, 400);
-  for (var i = 0; i < 4; i++) {
-    penRGB(255, 222, 173, 1);
-    dot(135);
-    move(135, 0);
+  penDown();
+  turnRight(90);
+  penRGB(255, 222, 173, 1);
+  penWidth(200);
+  moveForward(400);
   }
-}
 function drawSun() {
+  penUp();
   moveTo(randomNumber(0, 320), 1);
   penColor("yellow");
   dot(90);
 }
 function drawAllCactuses() {
-  for (var i = 0; i < 2; i++) {
-    moveTo(randomNumber(-25, 335), 450);
+    moveTo(randomNumber(-25, 140), 450);
+    penDown();
+    turnTo(0);
+    drawCactus();
+    penUp();
+    turnLeft(180);
+    moveTo(randomNumber(170, 330), 450);
     penDown();
     turnTo(0);
     drawCactus();
     penUp();
     turnLeft(180);
   }
+function drawSkyBackground() {
+  penRGB(randomNumber(0,70), randomNumber(150,220), randomNumber(230,255), 1);
+  dot(500);
+  penUp(0);
+  moveForward(0);
+  penWidth(150);
 }
 function drawCactus() {
   penColor("green");
@@ -81,6 +83,18 @@ function drawCactus() {
   moveBackward(40);
   turnTo(180);
   moveForward(14);
-  arcRight(65, 20);
-  
+  arcRight(90, 15);
+  moveForward(60);
+  arcRight(90, 10);
+  moveForward(90);
+  penWidth(30);
+  moveBackward(98);
+  turnTo(180);
+  turnRight(90);
+  penUp();
+  moveBackward(40);
+  turnLeft(90);
+  penDown();
+  penWidth(30);
+  moveBackward(150);
 }
